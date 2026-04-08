8aa56085e024daea72b5d9bcbf6e6972f81e3732
Разработчик 1 <dev1@yandex.ru>
2026-04-08 20:46:42 +0300

    Ветка feature-1 изменена

--- src/main/java/com/fooddelivery/App.java
+++ src/main/java/com/fooddelivery/App.java
@@ -5,6 +5,6 @@ package com.fooddelivery;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello from feature-1!");
     }
 }
